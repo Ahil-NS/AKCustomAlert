e009120304e4232aef6ab26739aec3df89877a50
a <eahilendran@gmail.com>
2019-01-07 11:32:05 +0530

    first initial commit

---
+++ iOSCustomAlert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSCustomAlert
+//
+//  Created by MacBook on 1/7/19.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
